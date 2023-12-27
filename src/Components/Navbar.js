805a90b0043f55e18de23fe4d3df604555c3aa49
Abdelrahman <86893144+abdelrahamn1@users.noreply.github.com>
2023-12-27 18:05:49 +0200

    Update Navbar.js
    update nav.js to make new navbar  "magic navbar, with new images

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,45 @@
-import logo from "../images/logo.png";
+import homeimg from "../images/home.png";
+import profileimg from "../images/profile.png";
+import exploreimg from "../images/explore.png";
+import statisticsimg from "../images/statistics.png";
 import { Link } from "react-router-dom";
+// import { IonIcon } from "@ionic/react";
+import { useState } from "react";
 import "../css/nav.css";
+
 function Navbar() {
+  const [activeItem, setActiveItem] = useState(0);
+
+  const handleItemClick = (index) => {
+    setActiveItem(index);
+  };
+
+  const items = [
+    { imagePath: homeimg, text: "Home", path: "/" },
+    { imagePath: profileimg, text: "Profile", path: "/profile" },
+    { imagePath: exploreimg, text: "Explore", path: "/explore" },
+    { imagePath: statisticsimg, text: "Statistics", path: "/statistics" },
+  ];
+
   return (
-    <div className="nav-bar">
-      <div className="container">
-        <div className="logo">
-          <img src={logo} alt="logo"></img>
-        </div>
-        <div className="links">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/explore">Expolre</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/statistics">Statistics</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <div className="navigation">
+      <ul>
+        {items.map((item, index) => (
+          <li
+            key={index}
+            className={`list ${activeItem === index ? "active" : ""}`}
+            onClick={() => handleItemClick(index)}
+          >
+            <Link to={item.path}>
+              <span className="icon">
+                <img src={item.imagePath} alt={item.text}></img>
+              </span>
+              <span className="text">{item.text}</span>
+            </Link>
+          </li>
+        ))}
+        <div className="indicator"></div>
+      </ul>
     </div>
   );
 }
