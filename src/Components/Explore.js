94adc2a37a2f8241a14e6e06ba299c65cb472fd8
Abdelrahman <86893144+abdelrahamn1@users.noreply.github.com>
2023-12-27 18:07:04 +0200

    Update Explore.js
    add some flex section in this page , until the real image and content come

--- src/Components/Explore.js
+++ src/Components/Explore.js
@@ -1,5 +1,105 @@
+import plantsvg from "../images/plantsvg.png";
 function Expolre() {
-  return <>Explore</>;
+  return (
+    <>
+      <div className="explore">
+        <div className="container">
+          <div className="flex-section">
+            <div className="flex">
+              <img src={plantsvg} alt="plant"></img>
+              <h2>Tomato</h2>
+              <p>
+                Loream Loream Loream Loream Loream Loream Loream Loream Loream
+                Loream Loream Loream Loream
+              </p>
+              <p>learn more!</p>
+            </div>
+            <div className="flex">
+              <img src={plantsvg} alt="plant"></img>
+              <h2>Tomato</h2>
+              <p>
+                Loream Loream Loream Loream Loream Loream Loream Loream Loream
+                Loream Loream Loream Loream
+              </p>
+              <p>learn more!</p>
+            </div>
+            <div className="flex">
+              <img src={plantsvg} alt="plant"></img>
+              <h2>Tomato</h2>
+              <p>
+                Loream Loream Loream Loream Loream Loream Loream Loream Loream
+                Loream Loream Loream Loream
+              </p>
+              <p>learn more!</p>
+            </div>
+            <div className="flex">
+              <img src={plantsvg} alt="plant"></img>
+              <h2>Tomato</h2>
+              <p>
+                Loream Loream Loream Loream Loream Loream Loream Loream Loream
+                Loream Loream Loream Loream
+              </p>
+              <p>learn more!</p>
+            </div>
+            <div className="flex">
+              <img src={plantsvg} alt="plant"></img>
+              <h2>Tomato</h2>
+              <p>
+                Loream Loream Loream Loream Loream Loream Loream Loream Loream
+                Loream Loream Loream Loream
+              </p>
+              <p>learn more!</p>
+            </div>
+            <div className="flex">
+              <img src={plantsvg} alt="plant"></img>
+              <h2>Tomato</h2>
+              <p>
+                Loream Loream Loream Loream Loream Loream Loream Loream Loream
+                Loream Loream Loream Loream
+              </p>
+              <p>learn more!</p>
+            </div>
+            <div className="flex">
+              <img src={plantsvg} alt="plant"></img>
+              <h2>Tomato</h2>
+              <p>
+                Loream Loream Loream Loream Loream Loream Loream Loream Loream
+                Loream Loream Loream Loream
+              </p>
+              <p>learn more!</p>
+            </div>
+            <div className="flex">
+              <img src={plantsvg} alt="plant"></img>
+              <h2>Tomato</h2>
+              <p>
+                Loream Loream Loream Loream Loream Loream Loream Loream Loream
+                Loream Loream Loream Loream
+              </p>
+              <p>learn more!</p>
+            </div>
+            <div className="flex">
+              <img src={plantsvg} alt="plant"></img>
+              <h2>Tomato</h2>
+              <p>
+                Loream Loream Loream Loream Loream Loream Loream Loream Loream
+                Loream Loream Loream Loream
+              </p>
+              <p>learn more!</p>
+            </div>
+            <div className="flex">
+              <img src={plantsvg} alt="plant"></img>
+              <h2>Tomato</h2>
+              <p>
+                Loream Loream Loream Loream Loream Loream Loream Loream Loream
+                Loream Loream Loream Loream
+              </p>
+              <p>learn more!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Expolre;
