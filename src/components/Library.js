e8ba0bf717c0ae3d1e2ab972d77b8c338b754b6c
Abdelrahman <86893144+abdelrahamn1@users.noreply.github.com>
2024-03-02 18:26:59 +0200

    Update Library.js

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,43 +1,20 @@
 import plantsData from "../components/data.json";
 import "../css/library.css";
-import React, { useState, useEffect } from "react";
 function Library() {
-  const {
-    plantName,
-    plantShortDescription,
-    plantMediumDescription,
-    plantLongDescription,
-    minimumdegrees,
-    stateofTemperature,
-    numberOfWater,
-    plantCareInstruct,
-  } = plantsData[0];
   return (
     <div className="library">
       <div className="container">
-        {plantsData.map((plant, index) => (
-          <div key={index} className="flex">
-            <h2>{plant.plantName}</h2>
-            <p>{plant.plantShortDescription}</p>
-            <p>
-              <strong> Description:</strong> {plant.plantMediumDescription}
-            </p>
-            <p>
-              <strong>About:</strong> {plant.plantLongDescription}
-            </p>
-            <p>
-              <strong>Minimum Temperature:</strong> {plant.minimumdegrees}
-            </p>
-            <p>
-              <strong>Required Sunlight:</strong> {plant.stateofTemperature}
-            </p>
-            <p>
-              <strong>Watering Needs:</strong> {plant.numberOfWater}
-            </p>
-            <p>
-              <strong>Care Instructions:</strong> {plant.plantCareInstruct}
-            </p>
-            <hr />
+        {plantsData.map((plant) => (
+          <div className="flex">
+            <li key={plant.id}>
+              <div>
+                <h2>{plant.common_name}</h2>
+                <p>Scientific Name: {plant.scientific_name}</p>
+                <p>Family: {plant.family}</p>
+              </div>
+              <img src={plant.image_url} alt={plant.common_name} />
+            </li>
+            <hr></hr>
           </div>
         ))}
       </div>
